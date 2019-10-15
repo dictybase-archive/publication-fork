179dd5000118870cb03f8243dd229417b9199ca3
wildlifehexagon <crossthebreeze@gmail.com>
2019-10-15 13:14:49 -0500

    fix: use correct /static path for sw location

--- src/sw-build.js
+++ src/sw-build.js
@@ -1,16 +1,14 @@
 const workboxBuild = require("workbox-build")
 // NOTE: This should be run *AFTER* all your assets are built
 const buildSW = () => {
-  // This will return a Promise
   return workboxBuild
     .injectManifest({
-      swSrc: "src/sw.js", // this is your sw template file
-      swDest: "build/static/sw.js", // this will be created in the build step
+      swSrc: "src/sw.js",
+      swDest: "build/static/sw.js",
       globDirectory: "build",
       globPatterns: ["**/*.{js,css,html,png}"],
     })
     .then(({ count, size, warnings }) => {
-      // Optionally, log any warnings and details.
       warnings.forEach(console.warn)
       console.log(`${count} files will be precached, totaling ${size} bytes.`)
     })
--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 if ("function" === typeof importScripts) {
   importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js",
+    "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js",
   )
   /* global workbox */
   if (workbox) {
